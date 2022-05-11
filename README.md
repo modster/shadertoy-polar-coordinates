682cdf51cdffcb52767b5729cc47ab28d3fc125f
E. M. Greeff <unmodster@gmail.com>
2022-05-11 05:47:41 -0600

    added polar coordinates shader program

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# shadertoy-template
-This repo is a template for rendering your shadetoy projects in a browser. Deploy to Github Pages by pushing to gh-pages branch.
+# shadertoy-polar-coordinates
 
-Demo: This site was built using [GitHub Pages](https://modster.github.io/shadertoy-template/).
+A simple shader that demonstrates the use of polar coordinates in shaders.
 
-Credit is given to the following people for their work: [gfxfundamentals/webgl2-fundamentals/graphs/contributors](https://github.com/gfxfundamentals/webgl2-fundamentals/graphs/contributors)
+Deploys to [GitHub Pages](https://modster.github.io/shadertoy-polar-coordinates/) when pushed to main branch.
 
-This is a modified version of the code from this repository: [webgl2fundamentals.org/webgl/lessons/webgl-shadertoy.html](https://webgl2fundamentals.org/webgl/lessons/webgl-shadertoy.html)
+Inspired by [BigWings](https://www.shadertoy.com/user/BigWIngs), aka the youtuber
+[Art of Code](https://www.youtube.com/c/TheArtofCodeIsCool/about) and his instructional video [Using Polar Coordinates to Create Art](https://youtu.be/r1UOB8NVE8I).
 
+
+A modified version of [webgl2fundamentals.org/webgl/lessons/webgl-shadertoy](https://webgl2fundamentals.org/webgl/lessons/webgl-shadertoy.html) is used to display the [Shadertoy](https://shadertoy.com) fragment shader.
--- index.js
+++ index.js
@@ -17,31 +17,43 @@ function main() {
   }
 
   const vs = `#version 300 es
-    // an attribute is an input (in) to a vertex shader.
-    // It will receive data from a buffer
-    in vec4 a_position;
+     // an attribute is an input (in) to a vertex shader.
+     // It will receive data from a buffer
+     in vec4 a_position;
 
-    // all shaders have a main function
-    void main() {
+     // all shaders have a main function
+     void main() {
 
-      // gl_Position is a special variable a vertex shader
-      // is responsible for setting
-      gl_Position = a_position;
-    }
-  `;
+       // gl_Position is a special variable a vertex shader
+       // is responsible for setting
+       gl_Position = a_position;
+     }
+   `;
 
   const fs = `#version 300 es
-    precision highp float;
+   precision highp float;
 
-    uniform vec2 u_resolution;
+   uniform vec2 iResolution;
 
-    // we need to declare an output for the fragment shader
-    out vec4 outColor;
+   void mainImage( out vec4 fragColor, in vec2 fragCoord )
+   {
 
-    void main() {
-      outColor = vec4(fract(gl_FragCoord.xy / u_resolution), 0, 1);
-    }
-  `;
+       vec2 uv = (fragCoord.xy-.5*iResolution.xy) / iResolution.y;
+       vec2 st = vec2(atan(uv.x, uv.y), length(uv));
+       uv = vec2 (st.x/6.2831+.5, st.y);
+       float x = uv.x*50.;
+       float m = min(fract(x), fract(1.-x));
+       float c = smoothstep(0., .1, m*.3+.2-uv.y);
+
+       fragColor = vec4(c);
+   }
+
+   out vec4 outColor;
+
+   void main() {
+     mainImage(outColor, gl_FragCoord.xy);
+   }
+   `;
 
   // setup GLSL program
   const program = webglUtils.createProgramFromSources(gl, [vs, fs]);
@@ -50,7 +62,8 @@ function main() {
   const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
 
   // look up uniform locations
-  const resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+  const resolutionLocation = gl.getUniformLocation(program, "iResolution");
+
 
   // Create a vertex array object (attribute state)
   const vao = gl.createVertexArray();
@@ -64,14 +77,14 @@ function main() {
   // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-  // fill it with a 2 triangles that cover clipspace
+  // fill it with a 2 triangles that cover clip space
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
     -1, -1,  // first triangle
-     1, -1,
-    -1,  1,
-    -1,  1,  // second triangle
-     1, -1,
-     1,  1,
+    1, -1,
+    -1, 1,
+    -1, 1,  // second triangle
+    1, -1,
+    1, 1,
   ]), gl.STATIC_DRAW);
 
   // Turn on the attribute
@@ -79,32 +92,39 @@ function main() {
 
   // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
   gl.vertexAttribPointer(
-      positionAttributeLocation,
-      2,          // 2 components per iteration
-      gl.FLOAT,   // the data is 32bit floats
-      false,      // don't normalize the data
-      0,          // 0 = move forward size * sizeof(type) each iteration to get the next position
-      0,          // start at the beginning of the buffer
+    positionAttributeLocation,
+    2,          // 2 components per iteration
+    gl.FLOAT,   // the data is 32bit floats
+    false,      // don't normalize the data
+    0,          // 0 = move forward size * sizeof(type) each iteration to get the next position
+    0,          // start at the beginning of the buffer
   );
 
-  webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+  function render(time) {
+    time *= 0.001;  // convert to seconds
 
-  // Tell WebGL how to convert from clip space to pixels
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
 
-  // Tell it to use our program (pair of shaders)
-  gl.useProgram(program);
+    // Tell WebGL how to convert from clip space to pixels
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-  // Bind the attribute/buffer set we want.
-  gl.bindVertexArray(vao);
+    // Tell it to use our program (pair of shaders)
+    gl.useProgram(program);
 
-  gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+    // Bind the attribute/buffer set we want.
+    gl.bindVertexArray(vao);
 
-  gl.drawArrays(
+    gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+
+    gl.drawArrays(
       gl.TRIANGLES,
       0,     // offset
       6,     // num vertices to process
-  );
+    );
+
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
 }
 
 main();
